6ca21538dfc5bf74a9a7c6754690762b1febaec1
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-01 05:05:07 +0300

    Add new project

---
+++ s21_string/s21_string/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  s21_string
+//
+//  Created by Арсентий Халимовский on 01.02.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
